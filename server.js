3bb3dbc5d1684a0a0ed3e014eb1c00e9a67df1ee
William McCotter <bj.mccotter@veteransunited.com>
2021-07-27 12:49:25 -0500

    changing it back

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ APP.use(CORS());
 
 APP.use(EXPRESS.static(PATH.join(__dirname, '/dist/mccotter-ui')));
 
-// APP.get('*', function (req, res) {
-//     res.sendFile(PATH.join(__dirname+'/dist/mccotter-ui/index.html'));
-// });
-
 APP.get('*', function (req, res) {
-    const index = PATH.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
+    res.sendFile(PATH.join(__dirname+'/dist/mccotter-ui/index.html'));
 });
+
+// APP.get('*', function (req, res) {
+//     const index = PATH.join(__dirname, 'build', 'index.html');
+//     res.sendFile(index);
+// });
 
 APP.listen(process.env.PORT || PORT);
 console.log('McCotter.net is rocking and rolling on port: ' + PORT);
