c483e422e2194590e7849694caf0873a0e2332e7
William McCotter <bj.mccotter@veteransunited.com>
2021-07-27 15:00:17 -0500

    containerized the service over HTTP

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:alpine AS builder
+
+ADD package.json /app/
+
+WORKDIR /app
+
+COPY . .
+
+EXPOSE 80
+
+RUN npm cache clean --force &&\
+    npm install &&\
+    npm run build
+
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const EXPRESS = require('express');
 const PATH = require('path');
 const APP = EXPRESS();
 const CORS = require('cors');
-const PORT = 8080
+const PORT = 80
 
 APP.use(CORS()); 
 
@@ -11,11 +11,6 @@ APP.use(EXPRESS.static(PATH.join(__dirname, '/dist/mccotter-ui')));
 APP.get('*', function (req, res) {
     res.sendFile(PATH.join(__dirname+'/dist/mccotter-ui/index.html'));
 });
-
-// APP.get('*', function (req, res) {
-//     const index = PATH.join(__dirname, 'build', 'index.html');
-//     res.sendFile(index);
-// });
 
 APP.listen(process.env.PORT || PORT);
 console.log('McCotter.net is rocking and rolling on port: ' + PORT);
