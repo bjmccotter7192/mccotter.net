36f869a0f8bc56dcbb212ffb77bf97f5de745a60
BJ McCotter <bjmccotter7192@gmail.com>
2022-01-28 18:11:10 -0600

    first push to new server

--- mccotter-net-react/src/App.tsx
+++ mccotter-net-react/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          BJ WAS HERE
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
